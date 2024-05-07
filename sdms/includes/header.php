9edb15bc77e67c8cef903d0b2ca269da0b96d62a
Ryniel Mark Lovino <117523796+neneney@users.noreply.github.com>
2024-05-08 05:26:02 +0800

    Initial commit

---
+++ sdms/includes/header.php
@@ -0,0 +1,36 @@
+
+<!-- Navbar -->
+<nav class="main-header navbar navbar-expand navbar-white navbar-light">
+  <!-- Left navbar links -->
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+    </li>
+    <li class="nav-item d-none d-sm-inline-block">
+      <a href="../dashboard.php" class="nav-link">Home</a>
+    </li>
+  </ul>
+
+
+  <!-- Right navbar links -->
+  <ul class="navbar-nav ml-auto">
+    <li class="nav-item">
+      <a class="nav-link" data-toggle="dropdown" href="#"><i class="fa-solid fa-user"></i> </a>
+      <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
+        <div class="dropdown-divider"></div>
+        <a href="profile.php" class="dropdown-item">
+        <i class="fa-solid fa-user mr-2"></i> profile
+        </a>
+        <div class="dropdown-divider"></div>
+        <a href="changepassword.php" class="dropdown-item">
+        <i class="fa-solid fa-gear mr-2"></i>password 
+        </a>
+        <div class="dropdown-divider"></div>
+        <a href="logout.php" class="dropdown-item">
+          <i class="fa-solid fa-right-from-bracket mr-2"></i> logout 
+        </a>
+      </div>
+    </li>
+  </ul>
+</nav>
+    <!-- /.navbar -->
